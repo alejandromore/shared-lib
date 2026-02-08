e0d0f09e2f95dbdad851dbf7a85c4c88645a3f6b
alejandromore <alejandromore@gmail.com>
2026-02-08 00:12:56 -0500

    update function

--- vars/helmDeployJenkins.groovy
+++ vars/helmDeployJenkins.groovy
@@ -8,25 +8,18 @@ def call(Map config = [:]) {
      * Parámetros esperados:
      *
      * dir              (obligatorio) → Directorio base donde vive el chart Helm
-     * chartDir         (obligatorio) → Carpeta del chart (ej: jenkins)
+     * chartDir         (obligatorio) → Carpeta del chart (ej: jekins)
      * kubeconfigCred   (obligatorio) → credentialId tipo "Secret file"
      * elbId            (obligatorio) → ID del ELB creado por Terraform
      * elbIp            (obligatorio) → IP pública del ELB
      * releaseName      (opcional)    → Nombre del release Helm default: jenkins
-     * namespace        (opcional)    → Namespace Kubernetes default: jenkins
+     * namespace        (opcional)    → Namespace Kubernetes default: jekins
      * healthEndpoint   (opcional)    → Endpoint HTTP para health check default: /login
-     * timeoutSeconds   (opcional)    → Timeout máximo de espera de Helm default: 300 segundos
-     *
-     * Flujo:
-     * 1. Valida acceso al cluster CCE
-     * 2. Ejecuta helm upgrade --install
-     * 3. Espera que el Deployment esté listo (--wait)
-     * 4. Realiza health check público vía ELB
-     *
+     * timeoutSeconds   (opcional)    → Timeout máximo de espera default: 300
      */
 
     def releaseName    = config.releaseName    ?: 'jenkins'
-    def namespace      = config.namespace      ?: 'jenkins'
+    def namespace      = config.namespace      ?: 'jekins'
     def healthEndpoint = config.healthEndpoint ?: '/login'
     def timeoutSeconds = config.timeoutSeconds ?: 300
 
